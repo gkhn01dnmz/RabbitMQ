7eed86af61183f285d06ca1bb82cf52f1f8c39cf
gkhn01dnmz <gokhan01donmez@gmail.com>
2023-12-25 08:51:42 +0300

    Added services  Add Transient project

---
+++ RabbitMQ.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infro.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RabbitMQ.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void  RegisterServices(IServiceCollection services)
+        {
+            //Doman Bus
+            services.AddTransient<IEventBus, RabbitMQBus>()
+        }
+    }
+}
